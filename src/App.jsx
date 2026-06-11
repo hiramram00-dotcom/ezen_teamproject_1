23548cde244a46e09067e7b605a2da08c82b6079
uiwnsy <uiwns19@gmail.com>
2026-06-11 15:26:48 +0900

    section spaces2

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-// import HeroSection from './sections/HeroSection/HeroSection'
-// import IntroSection from './sections/IntroSection/IntroSection'
+import HeroSection from './sections/HeroSection/HeroSection'
+import IntroSection from './sections/IntroSection/IntroSection'
 import SpacesSection from './sections/SpacesSection/SpacesSection'
 
 function App() {
   return (
     <main>
-      {/* <HeroSection />
-      <IntroSection /> */}
+      <HeroSection />
+      <IntroSection />
       <SpacesSection />
       {/* 다음 섹션들 순서대로 추가: Story, Snowman, Space, Collabo, Footer */}
     </main>
