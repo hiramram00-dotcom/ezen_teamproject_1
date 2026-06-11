d40d8c0a8b6c844b3e41526bb99615f5884ca09f
hiramram00-dotcom <hi.ramram.00@gmail.com>
2026-06-11 13:06:22 +0900

    1st/snowman

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import HeroSection from './sections/HeroSection/HeroSection'
-import IntroSection from './sections/IntroSection/IntroSection'
+// import HeroSection from './sections/HeroSection/HeroSection'
+// import IntroSection from './sections/IntroSection/IntroSection'
+import SnowmanSection2 from './sections/SnowmanSection2/SnowmanSection2'
 
 function App() {
   return (
     <main>
-      <HeroSection />
-      <IntroSection />
+      {/* <HeroSection />
+      <IntroSection /> */}
+      <SnowmanSection2 />
       {/* 다음 섹션들 순서대로 추가: Story, Snowman, Space, Collabo, Footer */}
     </main>
   )
