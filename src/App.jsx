72e013d401f4d2d9521d17725a5011b48770b92b
gayoung4000-blip <gayoung4000@gmail.com>
2026-06-15 18:29:12 +0900

    feat:story 추가 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import HeroSection from './sections/HeroSection/HeroSection'
 import IntroSection from './sections/IntroSection/IntroSection'
+import Story2Section from './sections/Story2Section/Story2Section'
 import StorySection from './sections/StorySection/StorySection'
 import SpaceSection from './sections/SpaceSection/SpaceSection'
 import StoryEndingSection from './sections/StoryEndingSection/StoryEndingSection'
@@ -14,6 +15,7 @@ function App() {
     <main>
       {<HeroSection />}
       {<IntroSection />}
+      <Story2Section />
       <StorySection />
       <SpaceSection />
       {<StoryEndingSection />}
